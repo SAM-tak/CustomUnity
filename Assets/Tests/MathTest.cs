6899d1414bfe21e60c9363bac7e67e56f0d677d6
ats.takasugi <ats.takasugi@trifort.jp>
2018-04-15 01:09:23 +0900

    Add Unit Test

---
+++ Assets/Tests/MathTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+namespace CustomUnity
+{
+    public class MathTest
+    {
+        [Test]
+        public void MathTestSimplePasses()
+        {
+            // Use the Assert class to test conditions.
+            Assert.AreEqual(Math.RubberStep(0, 1, 0, 1), 1);
+        }
+
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        [UnityTest]
+        public IEnumerator MathTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // yield to skip a frame
+            yield return null;
+        }
+    }
+}
