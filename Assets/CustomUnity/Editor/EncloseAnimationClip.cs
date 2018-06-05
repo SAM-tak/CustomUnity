f4fdda0e2cae29fdb37e4358b7d684905683ee5d
osamu takasugi <osamu.takasugi@gmail.com>
2018-06-06 02:14:30 +0900

    improve EcloseAnimationClip function

--- Assets/CustomUnity/Editor/EncloseAnimationClip.cs
+++ Assets/CustomUnity/Editor/EncloseAnimationClip.cs
@@ -1,29 +1,36 @@
-﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.Animations;
 using UnityEditor;
+using UnityEditor.Animations;
+using System.IO;
 
 namespace CustomUnity
 {
 	public class EncloseAnimationClip : EditorWindow
 	{
-		private AnimatorController controller;
+		AnimatorController controller;
 
 		string clipName;
 
-		[MenuItem("Assets/Enclose AnimationClip")]
+        const string menuString = "Assets/Enclose AnimationClip";
+        const int priority = 51;
+
+        [MenuItem(menuString, priority = priority)]
 		static void Create()
 		{
-			var window = EncloseAnimationClip.GetWindow(typeof(EncloseAnimationClip)) as EncloseAnimationClip;
-			if(Selection.activeObject is AnimatorController) {
-				window.controller = Selection.activeObject as AnimatorController;
-			}
+			var window = GetWindow<EncloseAnimationClip>(true, "Enclose AnimationClip", true);
+			window.controller = Selection.activeObject as AnimatorController;
 		}
 
-		void OnGUI()
+        [MenuItem(menuString, priority = priority, validate = true)]
+        static bool Validate()
+        {
+            return Selection.activeObject is AnimatorController;
+        }
+
+        void OnGUI()
 		{
-			EditorGUILayout.LabelField("Target Animator Controller");
+			EditorGUILayout.LabelField("Animator Controller");
 			controller = EditorGUILayout.ObjectField(controller, typeof(AnimatorController), false) as AnimatorController;
 
 			if(controller == null) return;
@@ -39,18 +46,46 @@ namespace CustomUnity
 				}
 			}
 
-			EditorGUILayout.Space();
-			EditorGUILayout.LabelField("Add new clip");
-			EditorGUILayout.BeginVertical("box");
-			
-			clipName = EditorGUILayout.TextField(clipName);
+			var dropArea = EditorGUILayout.BeginVertical("box");
+
+            EditorGUILayout.HelpBox("Drop AnimationClip to add here.", MessageType.Info, true);
+
+            switch(UnityEngine.Event.current.type) {
+            case EventType.DragUpdated:
+                if(dropArea.Contains(UnityEngine.Event.current.mousePosition)) {
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                }
+                break;
+            case EventType.DragPerform:
+                if(dropArea.Contains(UnityEngine.Event.current.mousePosition)) {
+                    DragAndDrop.AcceptDrag();
+                    foreach(AnimationClip animationClip in DragAndDrop.objectReferences) {
+                        if(clipList.Exists(item => item.name == animationClip.name) || string.IsNullOrEmpty(animationClip.name)) {
+                            EditorUtility.DisplayDialog("Error", "can't add an AnimationClip has duplicate or empty name", "OK");
+                        }
+                        else {
+                            var cloned = Instantiate(animationClip) as AnimationClip;
+                            cloned.name = animationClip.name;
+                            AssetDatabase.AddObjectToAsset(cloned, controller);
+                            AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(controller));
+                            AssetDatabase.Refresh();
+                        }
+                    }
+                }
+                break;
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Add new AnimationClip");
+
+            clipName = EditorGUILayout.TextField(clipName);
 			
 			if(clipList.Exists(item=> item.name == clipName ) || string.IsNullOrEmpty(clipName)) {
-				EditorGUILayout.LabelField("can't create duplicate names or empty");
-			}
-			else{
+                EditorGUILayout.HelpBox("can't create duplicate names or empty", MessageType.Warning);
+            }
+            else {
 				if(GUILayout.Button("Create")) {
-					var animationClip = UnityEditor.Animations.AnimatorController.AllocateAnimatorClip(clipName);
+					var animationClip = AnimatorController.AllocateAnimatorClip(clipName);
 					AssetDatabase.AddObjectToAsset(animationClip, controller);
 					AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(controller));
 					AssetDatabase.Refresh();
@@ -63,19 +98,29 @@ namespace CustomUnity
 			EditorGUILayout.Space();
 
 			EditorGUILayout.LabelField("Enclosed Clips");
-			EditorGUILayout.BeginVertical("box");
 
-			foreach(var removeClip in clipList) {
+			foreach(var enclosedClip in clipList) {
 				EditorGUILayout.BeginHorizontal();
 				
-				EditorGUILayout.LabelField(removeClip.name);
-				if(GUILayout.Button("Remove" , GUILayout.Width(100))) {
-					Object.DestroyImmediate(removeClip, true);
+				EditorGUILayout.LabelField(enclosedClip.name);
+				if(GUILayout.Button("Remove" , GUILayout.Width(80))) {
+					DestroyImmediate(enclosedClip, true);
 					AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(controller));
 				}
-				EditorGUILayout.EndHorizontal();
+                if(GUILayout.Button("Extract", GUILayout.Width(80))) {
+                    var cloned = Instantiate(enclosedClip) as AnimationClip;
+                    cloned.name = enclosedClip.name;
+                    var destinationPath = Path.Combine(Path.GetDirectoryName(AssetDatabase.GetAssetPath(controller)), cloned.name + ".anim");
+                    if(File.Exists(destinationPath)) {
+                        EditorUtility.DisplayDialog("Error", "\"" + destinationPath + "\" is already exists.", "OK");
+                    }
+                    else {
+                        Debug.Log("extract to " + destinationPath);
+                        AssetDatabase.CreateAsset(cloned, destinationPath);
+                    }
+                }
+                EditorGUILayout.EndHorizontal();
 			}
-			EditorGUILayout.EndVertical();
 		}
 	}
 }
