dd16186095d80fbe1a835df325c9b4637c3fe570
SAM-tak <osamu.takasugi@gmail.com>
2018-08-16 23:54:11 +0900

    Improve WaitForEnumerator

--- Assets/CustomUnity/WaitForEnumrators.cs
+++ Assets/CustomUnity/WaitForEnumrators.cs
@@ -4,7 +4,7 @@ using System.Linq;
 namespace CustomUnity
 {
     /// <summary>
-    /// move all Enumrators that given as parameter
+    /// move all Enumerators that given as parameter
     /// </summary>
     /// <usage>
     /// var op1 = AssetBundleManager.LoadAssetAsync<GameObject>("a", "obj");
@@ -18,18 +18,28 @@ namespace CustomUnity
             this.enumrators = enumrators;
         }
 
-        readonly IEnumerator[] enumrators;
+        public WaitForEnumerators(IEnumerable<IEnumerator> enumrators)
+        {
+            this.enumrators = enumrators;
+        }
+        
+        readonly IEnumerable<IEnumerator> enumrators;
 
         public object Current => null;
 
         public bool MoveNext()
         {
-            return !enumrators.All(x => !x.MoveNext());
+            return !enumrators.All(x => !MoveNextRecursively(x));
         }
 
         public void Reset()
         {
             foreach(var i in enumrators) i.Reset();
+        }
+
+        bool MoveNextRecursively(IEnumerator enumrator)
+        {
+            return enumerator.Current is IEnumerator && MoveNextReccursively((IEnumerator)enumerator.Current) ? true : enumerator.MoveNext();
         }
     }
 }
