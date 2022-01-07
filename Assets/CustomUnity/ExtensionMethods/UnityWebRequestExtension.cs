7b4993c85662be6435ccfe418eb634ebf8fbfd2f
osamu takasugi <osamu.takasugi@gmail.com>
2022-01-07 09:13:44 +0900

    Add UnityWebRequestExtension

---
+++ Assets/CustomUnity/ExtensionMethods/UnityWebRequestExtension.cs
@@ -0,0 +1,37 @@
+using UnityEngine.Networking;
+
+public static class UnityWebRequestExtension
+{
+    public static UnityWebRequest PostJson(string uri, string json)
+    {
+        var ret = UnityWebRequest.Post(uri, json);
+        ret.uploadHandler.contentType = "application/json";
+        return ret;
+    }
+    public static UnityWebRequest PutJson(string uri, string json)
+    {
+        var ret = UnityWebRequest.Put(uri, json);
+        ret.uploadHandler.contentType = "application/json";
+        return ret;
+    }
+
+    public static UnityWebRequest Post(string uri, byte[] payload, string contentType)
+    {
+        return new UnityWebRequest(
+            uri,
+            UnityWebRequest.kHttpVerbPOST,
+            new DownloadHandlerBuffer(),
+            new UploadHandlerRaw(payload) { contentType = contentType }
+        );
+    }
+
+    public static UnityWebRequest Put(string uri, byte[] payload, string contentType)
+    {
+        return new UnityWebRequest(
+            uri,
+            UnityWebRequest.kHttpVerbPOST,
+            new DownloadHandlerBuffer(),
+            new UploadHandlerRaw(payload) { contentType = contentType }
+        );
+    }
+}
