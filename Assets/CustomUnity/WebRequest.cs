bf3564340356130c1ef32dd4d87e665aa1a85813
osamu takasugi <osamu.takasugi@gmail.com>
2022-01-07 09:41:21 +0900

    fix error

---
+++ Assets/CustomUnity/WebRequest.cs
@@ -0,0 +1,41 @@
+using UnityEngine.Networking;
+
+namespace CustomUnity
+{
+    public static class WebRequest
+    {
+        public static UnityWebRequest PostJson(string uri, string json)
+        {
+            var ret = UnityWebRequest.Post(uri, json);
+            ret.uploadHandler.contentType = "application/json";
+            return ret;
+        }
+
+        public static UnityWebRequest PutJson(string uri, string json)
+        {
+            var ret = UnityWebRequest.Put(uri, json);
+            ret.uploadHandler.contentType = "application/json";
+            return ret;
+        }
+
+        public static UnityWebRequest Post(string uri, byte[] payload, string contentType)
+        {
+            return new UnityWebRequest(
+                uri,
+                UnityWebRequest.kHttpVerbPOST,
+                new DownloadHandlerBuffer(),
+                new UploadHandlerRaw(payload) { contentType = contentType }
+            );
+        }
+
+        public static UnityWebRequest Put(string uri, byte[] payload, string contentType)
+        {
+            return new UnityWebRequest(
+                uri,
+                UnityWebRequest.kHttpVerbPOST,
+                new DownloadHandlerBuffer(),
+                new UploadHandlerRaw(payload) { contentType = contentType }
+            );
+        }
+    }
+}
